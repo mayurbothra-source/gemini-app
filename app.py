542f6d9beadd67ac4cd4554eff918bd15ecd8687
mayurbothra-source <mayurbothra@gmail.com>
2025-12-23 20:30:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,9 @@ def index():
     return render_template_string(HTML, response=response)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
